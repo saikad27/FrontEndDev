48c8d1259c90cc085f6314e3a1552bee91fd3892
saikad27 <sairajkad501@icloud.com>
2026-07-09 17:47:45 +0530

    js object attributes inside html in jsx

--- 3_react_project/src/App.jsx
+++ 3_react_project/src/App.jsx
@@ -1,10 +1,12 @@
 
-const welcome = 'React';
+const welcome = {
+  greeting : "Hey",
+  title : "React"
+};
 function App() {
-  
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>{welcome.greeting} {welcome.title}</h1>
       <label htmlFor="search">Search : </label>
       <input id="search" type="text" />
     </div>
