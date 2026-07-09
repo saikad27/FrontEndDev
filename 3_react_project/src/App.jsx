bf58f810cdb6c5a5a559eecfaa2bd398bbf4bb6d
saikad27 <sairajkad501@icloud.com>
2026-07-09 17:50:32 +0530

    calling a js function inside jsx in curly braces

--- 3_react_project/src/App.jsx
+++ 3_react_project/src/App.jsx
@@ -3,10 +3,13 @@ const welcome = {
   greeting : "Hey",
   title : "React"
 };
+function getTitle(){
+  return 'React';
+}
 function App() {
   return (
     <div>
-      <h1>{welcome.greeting} {welcome.title}</h1>
+      <h1>{welcome.greeting} {getTitle()}</h1>
       <label htmlFor="search">Search : </label>
       <input id="search" type="text" />
     </div>
